dd48c01d1d45994181750b13eeb70f432df4ad0f
kanghua <kanghua151@gmail.com>
2017-07-06 13:31:29 +0800

    modify benchmarks from tushare instead of yahoo finance

--- zipline/data/benchmarks.py
+++ zipline/data/benchmarks.py
@@ -17,7 +17,7 @@ import pandas as pd
 
 import pandas_datareader.data as pd_reader
 
-
+'''
 def get_benchmark_returns(symbol, first_date, last_date):
     """
     Get a Series of benchmark returns from Google associated with `symbol`.
@@ -59,3 +59,36 @@ def get_benchmark_returns(symbol, first_date, last_date):
     data = data.fillna(method='ffill')
 
     return data.sort_index().tz_localize('UTC').pct_change(1).iloc[1:]
+'''
+from zipline.utils.calendars import get_calendar
+
+def get_benchmark_returns(symbol, start_date, end_date):
+    """
+    Get a Series of benchmark returns from Google Finance.
+    Returns a Series with returns from (start_date, end_date].
+    start_date is **not** included because we need the close from day N - 1 to
+    compute the returns for day N.
+    """
+    if symbol == "^GSPC":
+        symbol = "spy"
+
+    #print(symbol, start_date, end_date)
+    # benchmark_frame = web.DataReader(symbol, 'google', start_date, end_date).sort_index()
+    import tushare as ts
+    benchmark_frame = ts.get_h_data(
+        symbol,
+        start=start_date.strftime("%Y-%m-%d") if start_date != None else None,
+        end=end_date.strftime("%Y-%m-%d") if end_date != None else None,
+        retry_count=5,
+        pause=1
+    ).sort_index()
+    calendar = get_calendar("SHSZ")
+    sessions = calendar.sessions_in_range(start_date, end_date)
+    df = benchmark_frame.reindex(
+        sessions.tz_localize(None),
+        copy=False,
+        # ).fillna(0.0)["Close"].tz_localize('UTC').pct_change(1).iloc[1:]
+    ).fillna(0.0)["close"].tz_localize('UTC').pct_change(1).iloc[1:]
+
+    # x = df["Close"].sort_index().tz_localize('UTC').pct_change(1).iloc[1:]
+    return df
