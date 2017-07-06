637210377aaf039dff7274819e56988ead823537
kanghua <kanghua151@gmail.com>
2017-07-06 11:22:36 +0800

    modify for yahoo bundle with calendar - SHSZ

--- zipline/algorithm.py
+++ zipline/algorithm.py
@@ -289,7 +289,7 @@ class TradingAlgorithm(object):
         # If a schedule has been provided, pop it. Otherwise, use NYSE.
         self.trading_calendar = kwargs.pop(
             'trading_calendar',
-            get_calendar("NYSE")
+            get_calendar("SHSZ")
         )
 
         self.sim_params = kwargs.pop('sim_params', None)
@@ -529,7 +529,7 @@ class TradingAlgorithm(object):
         before_trading_start_minutes = days_at_time(
             self.sim_params.sessions,
             time(8, 45),
-            "US/Eastern"
+            "Asia/Shanghai"
         )
 
         return MinuteSimulationClock(
