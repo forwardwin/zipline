04670520c98c18c39f8c885098387e3ddc6da30a
kanghua <kanghua151@gmail.com>
2017-07-08 11:44:27 +0800

    emit env param in TradingEnviroment

--- zipline/utils/run_algo.py
+++ zipline/utils/run_algo.py
@@ -131,7 +131,7 @@ def _run(handle_data,
                 "invalid url %r, must begin with 'sqlite:///'" %
                 str(bundle_data.asset_finder.engine.url),
             )
-        env = TradingEnvironment(bm_symbol=bm_symbol, asset_db_path=connstr, environ=environ)
+        env = TradingEnvironment(bm_symbol=bm_symbol, asset_db_path=connstr)
         #env = TradingEnvironment(bm_symbol='000001',
         #                         exchange_tz='Asia/Shanghai',
         #                         trading_calendar=get_calendar('SHSZ'),
