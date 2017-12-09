3a0d853014e0de3feb8512272d04b1b0333437bb
kang <kanghua151@msn.com>
2017-12-09 11:08:48 +0800

    Update loader.py
    omit warning -- ‘Found TradingCalendar instances after zipline import’

--- zipline/data/loader.py
+++ zipline/data/loader.py
@@ -45,12 +45,12 @@ INDEX_MAPPING = {
 
 ONE_HOUR = pd.Timedelta(hours=1)
 
-from cn_stock_holidays.zipline.default_calendar import shsz_calendar
-nyse_cal = get_calendar('SHSZ')
+#from cn_stock_holidays.zipline.default_calendar import shsz_calendar
+#nyse_cal = get_calendar('SHSZ')
 
 #nyse_cal = get_calendar('NYSE')
-trading_day_nyse = nyse_cal.day
-trading_days_nyse = nyse_cal.all_sessions
+#trading_day_nyse = nyse_cal.day
+#trading_days_nyse = nyse_cal.all_sessions
 
 
 def last_modified_time(path):
